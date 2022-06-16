07c58eb1b218814f974e0c07c04564aca3076f6c
cjoiceSantos <carla.joice@hotmail.com>
2022-06-15 23:24:07 -0300

    initial commit

---
+++ data-processing.js
@@ -0,0 +1,479 @@
+const data = [
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 4,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 5,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 6,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 5,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 5,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 7,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 8,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 2,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino Fundamental completo",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 3,
+  },
+  {
+    quantidade_ocorrencias: 4,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino Fundamental completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino Fundamental completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino Fundamental completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 6,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 5,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 3,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 9,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Acima de 20 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino Fundamental incompleto",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Até 2 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 10 e 20 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 2 e 4 salários mínimos",
+    escolaridade: "Ensino superior completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino médio completo",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino médio incompleto",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 2,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Ensino superior incompleto",
+    mes_reposta: 10,
+  },
+  {
+    quantidade_ocorrencias: 1,
+    renda: "Entre 4 e 10 salários mínimos",
+    escolaridade: "Pós-graduação",
+    mes_reposta: 10,
+  },
+];
+
+function sanitizeData() {
+  const dataToBeSanitized = data;
+
+  const dataGroupedByIncome = [];
+
+  for (const item of dataToBeSanitized) {
+    const selectedIncome = item.renda;
+
+    const selectedIncomeFound = dataGroupedByIncome.find(
+      (el) => el.renda === selectedIncome
+    );
+
+    if (!!selectedIncomeFound) {
+      continue;
+    }
+
+    const dataWithThatIncome = dataToBeSanitized.filter(
+      (el) => el.renda === selectedIncome
+    );
+
+    let payload = {
+      renda: selectedIncome,
+      data: [],
+    };
+
+    dataWithThatIncome.forEach((el) =>
+      payload.data.push({
+        quantidade_ocorrencias: el.quantidade_ocorrencias,
+        escolaridade: el.escolaridade,
+        mes_reposta: el.mes_reposta,
+      })
+    );
+
+    dataGroupedByIncome.push(payload);
+  }
+
+  return dataGroupedByIncome;
+}
